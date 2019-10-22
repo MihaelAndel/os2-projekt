3c510bd6b7cfdbb72efa7a9601ada456d2a75354
Mihael Anđel <andel.mihael@gmail.com>
2019-10-22 21:30:57 +0200

    Dodane klase za funkcionalnost 1

---
+++ OS2-Projekt/OS2-Projekt/IKeyGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OS2_Projekt
+{
+    interface IKeyGenerator
+    {
+        List<int> GenerateKeys();
+        List<string> GenerateKeyStrings();
+    }
+}
