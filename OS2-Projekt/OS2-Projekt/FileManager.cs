59c7379bb81115b85b9242fa3223de33e00b7a51
Mihael Anđel <andel.mihael@gmail.com>
2019-10-23 19:04:53 +0200

    Implementacija FileManager klase
    Statička klasa koja služi za stvaranje i upravljanje datotekama. Trenutno je implementirana metoda za upisivanje generiranih ključeva u datoteku.

---
+++ OS2-Projekt/OS2-Projekt/FileManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace OS2_Projekt
+{
+    static class FileManager
+    {
+        public static string RootPath;
+
+        public static void WriteKeyToFile(string fileName, List<Tuple<string, byte[]>> keys)
+        {
+            StringKeyBuilder stringBuilder = new StringKeyBuilder();
+            StreamWriter file = File.CreateText(RootPath + fileName);
+
+            foreach(var key in keys)
+            {
+                file.WriteLine(stringBuilder.BuildString(key));
+            }
+
+            file.Close();
+
+        }
+    }
+}
