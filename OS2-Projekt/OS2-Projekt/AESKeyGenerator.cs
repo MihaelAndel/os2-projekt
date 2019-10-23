9013cb05dfd8ea65ba488ecbecfbfd1331cdcca5
Mihael Anđel <andel.mihael@gmail.com>
2019-10-23 12:48:13 +0200

    Implementacija AES i RSA key generatora
    AES generator vraća samo tajni ključ, dok RSA generatora vraća javni i privatni ključ. Generacija je implementirana na način da se svaki pojedini element ključa nalazi u Tuple strukturi gdje je jedan element labela za zapis u datoteci, a drugi element konkretan ključ.

--- OS2-Projekt/OS2-Projekt/AESKeyGenerator.cs
+++ OS2-Projekt/OS2-Projekt/AESKeyGenerator.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace OS2_Projekt
 {
     class AESKeyGenerator : IKeyGenerator
     {
+        private Aes keyProvider;
+        public List<Tuple<string, byte[]>> secretKey = null;
+
+        public AESKeyGenerator()
+        {
+            keyProvider = Aes.Create();
+        }
+
+        public void GenerateKeys()
+        {
+            secretKey = GenerateSecretKey();
+        }
+
+        private List<Tuple<string, byte[]>> GenerateSecretKey()
+        {
+            List<Tuple<string, byte[]>> keys = new List<Tuple<string, byte[]>>();
+            keys.Add(Tuple.Create("Tajni AES ključ", keyProvider.Key));
+            return keys;
+        }
     }
 }
