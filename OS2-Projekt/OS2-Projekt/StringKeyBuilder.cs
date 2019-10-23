fa90b71957b3e6149a1c209200a863a159f23aa1
Mihael Anđel <andel.mihael@gmail.com>
2019-10-23 12:06:55 +0200

    Promijenjen tip argumenta u odgovarajući tip

--- OS2-Projekt/OS2-Projekt/StringKeyBuilder.cs
+++ OS2-Projekt/OS2-Projekt/StringKeyBuilder.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +8,7 @@ namespace OS2_Projekt
 {
     class StringKeyBuilder
     {
-        public string BuildString(string text, int key)
+        public string BuildString(string text, byte[] key)
         {
             return "TO DO";
         }
