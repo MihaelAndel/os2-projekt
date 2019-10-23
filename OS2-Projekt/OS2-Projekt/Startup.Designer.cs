3c0e60cd7c62ec7b73de051ca4446a3384eeefd4
Mihael Anđel <andel.mihael@gmail.com>
2019-10-23 18:59:33 +0200

    Inicijalna forma - stvaranje datoteka s ključevima
    Dodan event handler za klik na gumb. Stvaraju se sve datoteke s potrebnim privatnim, javnim i tajnim ključevima.

--- OS2-Projekt/OS2-Projekt/Startup.Designer.cs
+++ OS2-Projekt/OS2-Projekt/Startup.Designer.cs
@@ -28,12 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.UIActionCreateKeyFiles = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // UIActionCreateKeyFiles
+            // 
+            this.UIActionCreateKeyFiles.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.UIActionCreateKeyFiles.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.UIActionCreateKeyFiles.FlatAppearance.BorderSize = 0;
+            this.UIActionCreateKeyFiles.Location = new System.Drawing.Point(215, 154);
+            this.UIActionCreateKeyFiles.Name = "UIActionCreateKeyFiles";
+            this.UIActionCreateKeyFiles.Size = new System.Drawing.Size(141, 62);
+            this.UIActionCreateKeyFiles.TabIndex = 0;
+            this.UIActionCreateKeyFiles.Text = "Stvori datoteke s ključevima";
+            this.UIActionCreateKeyFiles.UseVisualStyleBackColor = true;
+            this.UIActionCreateKeyFiles.Click += new System.EventHandler(this.UIActionCreateKeyFiles_Click);
+            // 
+            // Startup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.UIActionCreateKeyFiles);
+            this.Name = "Startup";
+            this.Text = "Operacijski Sustavi 2 - Projekt - Mihael Anđel";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button UIActionCreateKeyFiles;
     }
 }
--- OS2-Projekt/OS2-Projekt/Startup.cs
+++ OS2-Projekt/OS2-Projekt/Startup.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
 using System.Windows.Forms;
 
 namespace OS2_Projekt
@@ -15,6 +17,24 @@ namespace OS2_Projekt
         public Startup()
         {
             InitializeComponent();
+            FileManager.RootPath = @"C:\Users\andel\Documents\Repozitoriji\os2-projekt\datoteke\";
+        }
+
+        private void UIActionCreateKeyFiles_Click(object sender, EventArgs e)
+        {
+            RSA rsa = RSA.Create();
+            Aes aes = Aes.Create();
+
+            RSAKeyGenerator rsaGenerator = new RSAKeyGenerator(rsa);
+            AESKeyGenerator aesGenerator = new AESKeyGenerator(aes);
+
+            rsaGenerator.GenerateKeys();
+            aesGenerator.GenerateKeys();
+
+            FileManager.WriteKeyToFile("javni.txt", rsaGenerator.publicKey);
+            FileManager.WriteKeyToFile("privatni.txt", rsaGenerator.privateKey);
+            FileManager.WriteKeyToFile("tajni.txt", aesGenerator.secretKey);
+            
         }
     }
 }
