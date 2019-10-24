4c6d4d808ac79388f8470b8e92cfbad3a2100c42
Mihael Anđel <andel.mihael@gmail.com>
2019-10-24 14:07:11 +0200

    Statičke klase za dobavljanje ključeva

---
+++ OS2-Projekt/OS2-Projekt/RSAKeyProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace OS2_Projekt
+{
+    static class RSAKeyProvider
+    {
+        private static RSA key = null;
+
+        public static RSA ProvideKey()
+        {
+            if (key == null)
+            {
+                key = RSA.Create();
+            }
+            return key;
+        }
+
+    }
+}
