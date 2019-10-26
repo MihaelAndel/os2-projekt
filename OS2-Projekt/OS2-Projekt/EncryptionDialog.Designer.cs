4ef1f004da4ccbc12d237f6297d2cd888140cfc1
Mihael Anđel <andel.mihael@gmail.com>
2019-10-26 16:10:26 +0200

    EncryptionDialog forma
    Početak forme za kriptiranje i dekriptiranje odabrane datoteke

---
+++ OS2-Projekt/OS2-Projekt/EncryptionDialog.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace OS2_Projekt
+{
+    partial class EncryptionDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UIActionSymEncrypt = new System.Windows.Forms.Button();
+            this.UIActionAsymEncrypt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // UIActionSymEncrypt
+            // 
+            this.UIActionSymEncrypt.Location = new System.Drawing.Point(32, 24);
+            this.UIActionSymEncrypt.Name = "UIActionSymEncrypt";
+            this.UIActionSymEncrypt.Size = new System.Drawing.Size(300, 50);
+            this.UIActionSymEncrypt.TabIndex = 0;
+            this.UIActionSymEncrypt.Text = "Simetrično kriptiranje - odabir datoteke";
+            this.UIActionSymEncrypt.UseVisualStyleBackColor = true;
+            this.UIActionSymEncrypt.Click += new System.EventHandler(this.UIActionSymEncrypt_Click);
+            // 
+            // UIActionAsymEncrypt
+            // 
+            this.UIActionAsymEncrypt.Location = new System.Drawing.Point(349, 24);
+            this.UIActionAsymEncrypt.Name = "UIActionAsymEncrypt";
+            this.UIActionAsymEncrypt.Size = new System.Drawing.Size(300, 50);
+            this.UIActionAsymEncrypt.TabIndex = 1;
+            this.UIActionAsymEncrypt.Text = "Asimetrično kriptiranje - odabir datoteke";
+            this.UIActionAsymEncrypt.UseVisualStyleBackColor = true;
+            this.UIActionAsymEncrypt.Click += new System.EventHandler(this.UIActionAsymEncrypt_Click);
+            // 
+            // EncryptionDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.UIActionAsymEncrypt);
+            this.Controls.Add(this.UIActionSymEncrypt);
+            this.Name = "EncryptionDialog";
+            this.Text = "EncryptionDialog";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button UIActionSymEncrypt;
+        private System.Windows.Forms.Button UIActionAsymEncrypt;
+    }
+}
---
+++ OS2-Projekt/OS2-Projekt/EncryptionDialog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OS2_Projekt
+{
+    public partial class EncryptionDialog : Form
+    {
+        public EncryptionDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void UIActionSymEncrypt_Click(object sender, EventArgs e)
+        {
+            using(OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                if(openFileDialog.ShowDialog() == DialogResult.OK) { }
+            }
+        }
+
+        private void UIActionAsymEncrypt_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
