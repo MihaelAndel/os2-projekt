708d4915d7219f6034a676abfea864715c806678
Mihael Anđel <andel.mihael@gmail.com>
2019-10-26 20:09:27 +0200

    Forma za enkripciju i dekripciju datoteke
    Dodani gumbi i metode za odabir datoteke i kriptiranje sadržaja odabrane datoteke simetričnim i asimetričnim algoritmima.

--- OS2-Projekt/OS2-Projekt/EncryptionDialog.Designer.cs
+++ OS2-Projekt/OS2-Projekt/EncryptionDialog.Designer.cs
@@ -30,35 +30,50 @@
         {
             this.UIActionSymEncrypt = new System.Windows.Forms.Button();
             this.UIActionAsymEncrypt = new System.Windows.Forms.Button();
+            this.UIActionSelectFile = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // UIActionSymEncrypt
             // 
-            this.UIActionSymEncrypt.Location = new System.Drawing.Point(32, 24);
+            this.UIActionSymEncrypt.Location = new System.Drawing.Point(24, 20);
+            this.UIActionSymEncrypt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.UIActionSymEncrypt.Name = "UIActionSymEncrypt";
-            this.UIActionSymEncrypt.Size = new System.Drawing.Size(300, 50);
+            this.UIActionSymEncrypt.Size = new System.Drawing.Size(225, 41);
             this.UIActionSymEncrypt.TabIndex = 0;
-            this.UIActionSymEncrypt.Text = "Simetrično kriptiranje - odabir datoteke";
+            this.UIActionSymEncrypt.Text = "Simetrično kriptiranje datoteke";
             this.UIActionSymEncrypt.UseVisualStyleBackColor = true;
             this.UIActionSymEncrypt.Click += new System.EventHandler(this.UIActionSymEncrypt_Click);
             // 
             // UIActionAsymEncrypt
             // 
-            this.UIActionAsymEncrypt.Location = new System.Drawing.Point(349, 24);
+            this.UIActionAsymEncrypt.Location = new System.Drawing.Point(262, 20);
+            this.UIActionAsymEncrypt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.UIActionAsymEncrypt.Name = "UIActionAsymEncrypt";
-            this.UIActionAsymEncrypt.Size = new System.Drawing.Size(300, 50);
+            this.UIActionAsymEncrypt.Size = new System.Drawing.Size(225, 41);
             this.UIActionAsymEncrypt.TabIndex = 1;
-            this.UIActionAsymEncrypt.Text = "Asimetrično kriptiranje - odabir datoteke";
+            this.UIActionAsymEncrypt.Text = "Asimetrično kriptiranje datoteke";
             this.UIActionAsymEncrypt.UseVisualStyleBackColor = true;
             this.UIActionAsymEncrypt.Click += new System.EventHandler(this.UIActionAsymEncrypt_Click);
             // 
+            // UIActionSelectFile
+            // 
+            this.UIActionSelectFile.Location = new System.Drawing.Point(24, 106);
+            this.UIActionSelectFile.Name = "UIActionSelectFile";
+            this.UIActionSelectFile.Size = new System.Drawing.Size(225, 41);
+            this.UIActionSelectFile.TabIndex = 2;
+            this.UIActionSelectFile.Text = "Odabir datoteke za kriptiranje";
+            this.UIActionSelectFile.UseVisualStyleBackColor = true;
+            this.UIActionSelectFile.Click += new System.EventHandler(this.UIActionSelectFile_Click);
+            // 
             // EncryptionDialog
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.UIActionSelectFile);
             this.Controls.Add(this.UIActionAsymEncrypt);
             this.Controls.Add(this.UIActionSymEncrypt);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "EncryptionDialog";
             this.Text = "EncryptionDialog";
             this.ResumeLayout(false);
@@ -69,5 +84,6 @@
 
         private System.Windows.Forms.Button UIActionSymEncrypt;
         private System.Windows.Forms.Button UIActionAsymEncrypt;
+        private System.Windows.Forms.Button UIActionSelectFile;
     }
 }
--- OS2-Projekt/OS2-Projekt/EncryptionDialog.cs
+++ OS2-Projekt/OS2-Projekt/EncryptionDialog.cs
@@ -6,12 +6,17 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
+using System.Security.Cryptography;
 
 namespace OS2_Projekt
 {
     public partial class EncryptionDialog : Form
     {
+
+        private string fileContent;
+
         public EncryptionDialog()
         {
             InitializeComponent();
@@ -19,15 +24,43 @@ namespace OS2_Projekt
 
         private void UIActionSymEncrypt_Click(object sender, EventArgs e)
         {
-            using(OpenFileDialog openFileDialog = new OpenFileDialog())
-            {
-                if(openFileDialog.ShowDialog() == DialogResult.OK) { }
-            }
+            Aes aesKey = AESKeyProvider.ProvideKey();
+            var encryptor = aesKey.CreateEncryptor();
+
+            byte[] content = Encoding.UTF8.GetBytes(fileContent);
+            byte[] encryptedBytes = encryptor.TransformFinalBlock(content, 0, content.Length);
+
+            string encryptedContent = Convert.ToBase64String(encryptedBytes);
+            FileManager.WriteTextToFile("simetricna-enkripcija.txt", encryptedContent);
         }
 
         private void UIActionAsymEncrypt_Click(object sender, EventArgs e)
         {
+            RSACryptoServiceProvider rsaKey = new RSACryptoServiceProvider();
 
+            rsaKey.ImportParameters(RSAKeyProvider.ProvideKey().ExportParameters(true));
+
+            byte[] content = Encoding.UTF8.GetBytes(fileContent);
+            byte[] encryptedBytes = rsaKey.Encrypt(content, true); 
+
+            string encryptedContent = Convert.ToBase64String(encryptedBytes);
+            FileManager.WriteTextToFile("asimetricna-enkripcija.txt", encryptedContent);
+        }
+
+        private void UIActionSelectFile_Click(object sender, EventArgs e)
+        {
+            string pathToFile = FileManager.OpenFile();
+            string content = FileManager.ReadTextFile(pathToFile);
+
+            if(content.Length <= 0)
+            {
+                MessageBox.Show("Učitana datoteka nema sadržaja!");
+                return;
+            }
+            else
+            {
+                fileContent = content;
+            }
         }
     }
 }
