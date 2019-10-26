4204d54aa6c7bc8387bdc290539f9535257f9032
Mihael Anđel <andel.mihael@gmail.com>
2019-10-26 20:08:41 +0200

    Unaprijeđena FileManager klasa
    Dodane metode za kreiranje datoteke, pisanje teksta u datoteku i čitanje datoteke.

--- OS2-Projekt/OS2-Projekt/FileManager.cs
+++ OS2-Projekt/OS2-Projekt/FileManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using System.IO;
 
 namespace OS2_Projekt
@@ -13,7 +14,7 @@ namespace OS2_Projekt
 
         public static void WriteKeyToFile(string fileName, List<Tuple<string, byte[]>> keys)
         {
-            string fullPath = RootPath + fileName;
+            string fullPath = CreateFile(fileName);
             using (StreamWriter file = new StreamWriter(fullPath))
             {
                 MyStringBuilder strBuilder = new MyStringBuilder();
@@ -23,6 +24,39 @@ namespace OS2_Projekt
                     file.WriteLine(strBuilder.BuildKeyString(key));
                 }
             }
-        }       
+        }
+
+        public static void WriteTextToFile(string fileName, string text)
+        {
+            string fullPath = CreateFile(fileName);
+
+            using (StreamWriter file = new StreamWriter(fullPath))
+            {
+                file.WriteLine(text);
+            }
+        }
+
+        private static string CreateFile(string fileName)
+        {
+            string fullPath = RootPath + fileName;
+
+            return fullPath;
+        }
+
+        public static string OpenFile()
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    return openFileDialog.FileName;
+                } else return "";
+            }
+        }
+
+        public static string ReadTextFile(string pathToFile)
+        {
+            return File.ReadAllText(pathToFile);
+        }
     }
 }
