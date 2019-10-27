d3dc9fcfb5c4fd0d3b03f80ac29323676aebaf2c
Mihael Anđel <andel.mihael@gmail.com>
2019-10-27 17:36:15 +0100

    Dodane metode za dekripciju sadržaja datoteke
    Također je promijenjena metoda za enkripciju AES algoritmom. Za enkripciju i dekripciju AES algoritmom koriste se razčičiti Stream objekti koji prenose bajtove. Kod RSA algoritma nije potrebno koristiti ovakav način kriptiranja i dekriptiranja.

--- OS2-Projekt/OS2-Projekt/EncryptionDialog.Designer.cs
+++ OS2-Projekt/OS2-Projekt/EncryptionDialog.Designer.cs
@@ -31,12 +31,14 @@
             this.UIActionSymEncrypt = new System.Windows.Forms.Button();
             this.UIActionAsymEncrypt = new System.Windows.Forms.Button();
             this.UIActionSelectFile = new System.Windows.Forms.Button();
+            this.UIActionSymDecrypt = new System.Windows.Forms.Button();
+            this.UIActionAsymDecrypt = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // UIActionSymEncrypt
             // 
-            this.UIActionSymEncrypt.Location = new System.Drawing.Point(24, 20);
-            this.UIActionSymEncrypt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.UIActionSymEncrypt.Location = new System.Drawing.Point(37, 157);
+            this.UIActionSymEncrypt.Margin = new System.Windows.Forms.Padding(2);
             this.UIActionSymEncrypt.Name = "UIActionSymEncrypt";
             this.UIActionSymEncrypt.Size = new System.Drawing.Size(225, 41);
             this.UIActionSymEncrypt.TabIndex = 0;
@@ -46,8 +48,8 @@
             // 
             // UIActionAsymEncrypt
             // 
-            this.UIActionAsymEncrypt.Location = new System.Drawing.Point(262, 20);
-            this.UIActionAsymEncrypt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.UIActionAsymEncrypt.Location = new System.Drawing.Point(340, 157);
+            this.UIActionAsymEncrypt.Margin = new System.Windows.Forms.Padding(2);
             this.UIActionAsymEncrypt.Name = "UIActionAsymEncrypt";
             this.UIActionAsymEncrypt.Size = new System.Drawing.Size(225, 41);
             this.UIActionAsymEncrypt.TabIndex = 1;
@@ -57,7 +59,7 @@
             // 
             // UIActionSelectFile
             // 
-            this.UIActionSelectFile.Location = new System.Drawing.Point(24, 106);
+            this.UIActionSelectFile.Location = new System.Drawing.Point(37, 63);
             this.UIActionSelectFile.Name = "UIActionSelectFile";
             this.UIActionSelectFile.Size = new System.Drawing.Size(225, 41);
             this.UIActionSelectFile.TabIndex = 2;
@@ -65,15 +67,37 @@
             this.UIActionSelectFile.UseVisualStyleBackColor = true;
             this.UIActionSelectFile.Click += new System.EventHandler(this.UIActionSelectFile_Click);
             // 
+            // UIActionSymDecrypt
+            // 
+            this.UIActionSymDecrypt.Location = new System.Drawing.Point(37, 203);
+            this.UIActionSymDecrypt.Name = "UIActionSymDecrypt";
+            this.UIActionSymDecrypt.Size = new System.Drawing.Size(225, 41);
+            this.UIActionSymDecrypt.TabIndex = 3;
+            this.UIActionSymDecrypt.Text = "Simetrično dekriptiranje";
+            this.UIActionSymDecrypt.UseVisualStyleBackColor = true;
+            this.UIActionSymDecrypt.Click += new System.EventHandler(this.UIActionSymDecrypt_Click);
+            // 
+            // UIActionAsymDecrypt
+            // 
+            this.UIActionAsymDecrypt.Location = new System.Drawing.Point(340, 203);
+            this.UIActionAsymDecrypt.Name = "UIActionAsymDecrypt";
+            this.UIActionAsymDecrypt.Size = new System.Drawing.Size(225, 41);
+            this.UIActionAsymDecrypt.TabIndex = 4;
+            this.UIActionAsymDecrypt.Text = "Asimetrično dekriptiranje";
+            this.UIActionAsymDecrypt.UseVisualStyleBackColor = true;
+            this.UIActionAsymDecrypt.Click += new System.EventHandler(this.UIActionAsymDecrypt_Click);
+            // 
             // EncryptionDialog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.UIActionAsymDecrypt);
+            this.Controls.Add(this.UIActionSymDecrypt);
             this.Controls.Add(this.UIActionSelectFile);
             this.Controls.Add(this.UIActionAsymEncrypt);
             this.Controls.Add(this.UIActionSymEncrypt);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "EncryptionDialog";
             this.Text = "EncryptionDialog";
             this.ResumeLayout(false);
@@ -85,5 +109,7 @@
         private System.Windows.Forms.Button UIActionSymEncrypt;
         private System.Windows.Forms.Button UIActionAsymEncrypt;
         private System.Windows.Forms.Button UIActionSelectFile;
+        private System.Windows.Forms.Button UIActionSymDecrypt;
+        private System.Windows.Forms.Button UIActionAsymDecrypt;
     }
 }
--- OS2-Projekt/OS2-Projekt/EncryptionDialog.cs
+++ OS2-Projekt/OS2-Projekt/EncryptionDialog.cs
@@ -24,43 +24,90 @@ namespace OS2_Projekt
 
         private void UIActionSymEncrypt_Click(object sender, EventArgs e)
         {
+            string encryptedContent = "";
+
             Aes aesKey = AESKeyProvider.ProvideKey();
-            var encryptor = aesKey.CreateEncryptor();
+            var encryptor = aesKey.CreateEncryptor(aesKey.Key, aesKey.IV);
 
-            byte[] content = Encoding.UTF8.GetBytes(fileContent);
-            byte[] encryptedBytes = encryptor.TransformFinalBlock(content, 0, content.Length);
+            using (MemoryStream msEncrypt = new MemoryStream())
+            {
+                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                    {
+                        swEncrypt.Write(fileContent);
+                    }
+                }
+                encryptedContent = Convert.ToBase64String(msEncrypt.ToArray());
+            }
 
-            string encryptedContent = Convert.ToBase64String(encryptedBytes);
             FileManager.WriteTextToFile("simetricna-enkripcija.txt", encryptedContent);
         }
 
-        private void UIActionAsymEncrypt_Click(object sender, EventArgs e)
-        {
-            RSACryptoServiceProvider rsaKey = new RSACryptoServiceProvider();
-
-            rsaKey.ImportParameters(RSAKeyProvider.ProvideKey().ExportParameters(true));
-
-            byte[] content = Encoding.UTF8.GetBytes(fileContent);
-            byte[] encryptedBytes = rsaKey.Encrypt(content, true); 
-
-            string encryptedContent = Convert.ToBase64String(encryptedBytes);
-            FileManager.WriteTextToFile("asimetricna-enkripcija.txt", encryptedContent);
-        }
 
         private void UIActionSelectFile_Click(object sender, EventArgs e)
         {
             string pathToFile = FileManager.OpenFile();
             string content = FileManager.ReadTextFile(pathToFile);
 
-            if(content.Length <= 0)
+            if (content.Length <= 0)
             {
                 MessageBox.Show("Učitana datoteka nema sadržaja!");
                 return;
-            }
-            else
+            } else
             {
                 fileContent = content;
             }
+        }
+
+        private void UIActionSymDecrypt_Click(object sender, EventArgs e)
+        {
+            string decryptedContent = "";
+            string encryptedContent = FileManager.ReadTextFile(FileManager.RootPath + "simetricna-enkripcija.txt");
+
+            Aes aesKey = AESKeyProvider.ProvideKey();
+            var decryptor = aesKey.CreateDecryptor(aesKey.Key, aesKey.IV);
+
+            using (MemoryStream msDecrypt = new MemoryStream(Convert.FromBase64String(encryptedContent)))
+            {
+                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                {
+                    using (StreamReader swDecrypt = new StreamReader(csDecrypt))
+                    {
+                        decryptedContent = swDecrypt.ReadToEnd();
+                    }
+                }
+            }
+            FileManager.WriteTextToFile("simetricna-dekripcija.txt", decryptedContent);
+        }
+
+        private void UIActionAsymEncrypt_Click(object sender, EventArgs e)
+        {
+            RSACryptoServiceProvider rsaKey = new RSACryptoServiceProvider(2048);
+
+            rsaKey.ImportParameters(RSAKeyProvider.ProvideKey().ExportParameters(true));
+
+            byte[] content = Encoding.UTF8.GetBytes(fileContent);
+            byte[] encryptedBytes = rsaKey.Encrypt(content, true);
+
+            string encryptedContent = Convert.ToBase64String(encryptedBytes);
+            FileManager.WriteTextToFile("asimetricna-enkripcija.txt", encryptedContent);
+        }
+
+        private void UIActionAsymDecrypt_Click(object sender, EventArgs e)
+        {
+            RSACryptoServiceProvider rsaKey = new RSACryptoServiceProvider(2048);
+            rsaKey.ImportParameters(RSAKeyProvider.ProvideKey().ExportParameters(true));
+
+            string fullPath = FileManager.RootPath + "asimetricna-enkripcija.txt";
+            string encryptedContent = FileManager.ReadTextFile(fullPath);
+
+            byte[] encryptedBytes = Convert.FromBase64String(encryptedContent);
+            byte[] decryptedBytes = rsaKey.Decrypt(encryptedBytes, true);
+
+            string decryptedContent = Encoding.UTF8.GetString(decryptedBytes);
+            FileManager.WriteTextToFile("asimetricna-dekripcija.txt", decryptedContent);
+
         }
     }
 }
