ae0084a4e92948123308d16039d621c773c2354c
Mihael Anđel <andel.mihael@gmail.com>
2019-10-29 08:38:21 +0100

    Sažetak sadržaja datoteke
    Dodana klasa za računanje sažetka nekog teksta. Dodana metoda za stvaranje datoteke iz izračunatog sažetka.

---
+++ OS2-Projekt/OS2-Projekt/FileHasher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace OS2_Projekt
+{
+    class FileHasher
+    {
+        public string HashFile(string text)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+            byte[] hashedBytes = null;
+            using(var hasher = new SHA512Managed())
+            {
+                hashedBytes = hasher.ComputeHash(bytes);
+            }
+
+            string hashedText = Convert.ToBase64String(hashedBytes);
+            
+            return hashedText;
+        }
+    }
+}
